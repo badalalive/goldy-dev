4c18cde7d57cca005b36ae9b9674f7e3d9ac3fd6
badalalive <badal@bucle.dev>
2024-02-04 11:44:12 +0530

    feat: not-found feature added

---
+++ src/app/not-found.tsx
@@ -0,0 +1,3 @@
+export default function Custom404() {
+    return <div className="flex items-center justify-center h-screen"><img src='/not_found.svg' className="w-1/5" alt='logo'/> <h1 className="text-white text-4xl">-Page Not Found</h1></div>
+}
