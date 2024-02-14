fd6412b45d74864199c2c7fc9f17c08801a17636
badalalive <badal@bucle.dev>
2024-02-14 11:53:25 +0530

    mod: vesting lock - unlock functionality added

---
+++ src/app/components/Portfolio/lock-unclock-modal.tsx
@@ -0,0 +1,58 @@
+import {formatEtherToFloat, formatNumber} from "@/app/utills/common.util";
+import Link from "next/link";
+import React from "react";
+import Spinner from "@/app/components/Loader/spinner.loader";
+
+interface LockUnlockModalProps {
+    title: string,
+    value: string,
+    info: string,
+    buttonName: string,
+    approvalAction: (value: string) => void,
+    createAction: (value: string, time: number) => void,
+    status: boolean,
+    isLoading: boolean,
+    isVestingLoading: boolean,
+    isTransactionLoading: boolean
+}
+export default function LockUnlockModal (props: LockUnlockModalProps) {
+    const [amount, setAmount] = React.useState<string>("0");
+    console.log("status =>", props.status);
+    console.log("isLoading =>", props.isLoading);
+    console.log("isTransactionLoading =>", props.isTransactionLoading);
+    console.log("isVestingLoading =>", props.isVestingLoading);
+    React.useEffect(() => {
+        if(props.status && !props.isLoading) {
+            console.log("Success =>", props.status, Math.floor((new Date().getTime() + 5 * 60 * 1000) / 1000));
+            props.createAction(amount, Math.floor((new Date().getTime() + 5 * 60 * 1000) / 1000));
+        }
+    },[props.status, props.isLoading, props.isTransactionLoading]);
+    return (
+        <div className="fixed inset-0 backdrop-blur-xl flex justify-center items-center">
+            <div className="w-1/2 text-white border-4 border-[#FFD678] py-6 rounded-md  bg-[#272727]">
+                <div className=" pl-12 pr-36">
+                    <h1 className="text-xl font-bold">{props.title}</h1>
+                    <br/>
+                    <p className="font-semibold">Total Coins: <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#be9f59] to-[#433420]">{formatNumber(formatEtherToFloat(props.value ?? 0))}</span></p>
+                    <p className="text-[#999999]">Tokens can only be unlocked after a period of 1 year, calculated from the purchasing date.</p>
+                    <br/>
+                    <p className="font-semibold">Enter Amount</p>
+                    <input type="text" value={amount} placeholder={String(0)} className="w-full bg-transparent border-b border-grey focus:border-none focus-visible:border-none" onChange={(e) => setAmount(e.target.value)}/>
+                    <br/>
+                    <br/>
+                </div>
+                <div>
+                    <p className="text-xs text-center">{props.info}</p>
+                    <div className="flex justify-center">
+                        <button className="bg-gradient-to-r from-[#FED678] to-[#9B6128] px-12 py-2 text-black mt-3 rounded font-bold" disabled={props.isLoading} onClick={() => {
+                            props.approvalAction(amount)
+                        }}>{props.isLoading || props.isTransactionLoading || props.isVestingLoading ? <Spinner/> : props.buttonName}</button>
+                    </div>
+                    <Link href="/portfolio">
+                        <p className="text-center pt-4">Cancel</p>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/app/components/Portfolio/vesting.tsx
+++ src/app/components/Portfolio/vesting.tsx
@@ -1,18 +1,86 @@
 'use client';
 import {formatEtherToFloat, formatNumber} from "@/app/utills/common.util";
-import {useAccount} from "wagmi";
+import {useAccount, useContractReads, useContractWrite, useWaitForTransaction} from "wagmi";
 import {useCustomContractRead, useCustomContractReadWithArg} from "@/app/utills/contract.calls.helper";
-import {GoldyAddress, GoldyPriceOracleAddress} from "@/app/config/address/contract.address";
+import {GoldyAddress, GoldyPriceOracleAddress, IcoAddress, VestingAddress} from "@/app/config/address/contract.address";
 import {Erc20TokenAbi} from "@/app/config/abi/erc20.token.abi";
 import {GoldyPriceOracleAbi} from "@/app/config/abi/goldy.price.oracle.abi";
 import React from "react";
+import {VestingAbi} from "@/app/config/abi/vesting.abi";
+import Spinner from "@/app/components/Loader/spinner.loader";
+import Link from "next/link";
+import {useSearchParams} from "next/navigation";
+import LockUnlockModal from "@/app/components/Portfolio/lock-unclock-modal";
+import { ethers } from 'ethers';
 
 export default function Vesting() {
     const { address, isConnected } = useAccount();
+    const [ list, setList] = React.useState<any[]>([]);
     const { data: goldyBalance, isError: isGoldyBalanceError } = useCustomContractReadWithArg(GoldyAddress, Erc20TokenAbi, "balanceOf", [address], isConnected);
+    const { data: vestingCount, isError: isVestingCountError } = useCustomContractRead(VestingAddress, VestingAbi, "_vestingPoolIdTracker", isConnected);
+    const { data: vestingPools, isError, isLoading: isVestingPoolsLoading } = useContractReads({
+        contracts: list.map((index) => ({
+            address: VestingAddress,
+            abi: VestingAbi,
+            functionName: 'vestingPools',
+            args: [index],
+        })), enabled: true
+    })
+    const { data: withdrawAvailable, isError: isWithdrawAvailableError, isLoading: isWithdrawAvailableLoading } = useContractReads({
+        contracts: list.map((index) => ({
+            address: VestingAddress,
+            abi: VestingAbi,
+            functionName: 'availableToWithdraw',
+            args: [vestingPools?.at(index)?.result],
+        })), enabled: true
+    })
+    console.log("withdrawAvailable =>", withdrawAvailable);
     const [locked, setLocked ] = React.useState<number>(0);
+    const searchParams = useSearchParams();
+    const lock = searchParams.get("lock");
+    const unlock = searchParams.get("unlock");
+    const { data, isLoading, isSuccess, error,  write } = useContractWrite({
+        address: GoldyAddress,
+        abi: Erc20TokenAbi,
+        functionName: 'approve',
+    })
+    const { data: vestingData, isLoading: isVestingLoading, isSuccess: isIcoSuccess, error: icoError,  write: vestingWrite } = useContractWrite({
+        address: VestingAddress,
+        abi: VestingAbi,
+        functionName: 'create',
+    })
+    const {isSuccess: isTransactionSuccess, isFetched: isTransactionFetched, isLoading: isTransactionLoading} = useWaitForTransaction({
+        hash: data?.hash,
+    })
+    React.useEffect(() => {
+        const _list = [];
+        for (let i = 0; i < Number(vestingCount); i++) {
+            _list.push(i);
+        }
+        setList(_list);
+        setLocked(0);
+        console.log("Vesting pools =>", vestingPools);
+        vestingPools?.map((pool: any) => {
+            setLocked((currentValue) => {
+                return currentValue + Number(formatNumber(formatEtherToFloat(pool?.result[5])));
+            })
+        })
+    }, [vestingCount, vestingPools])
+    function callApproval(value: string) {
+        write({
+            args: [ VestingAddress, ethers.parseUnits(String(value), 18) ]
+        })
+    }
+    function callCreate(value: string, time: number) {
+        vestingWrite({
+            args: [ 0, time, 10000, 0, ethers.parseUnits(String(value), 18), address, GoldyAddress]
+        })
+    }
     return (
-        <div className="flex flex-col mt-24 mx-16 bg-[#272727] rounded-md">
+        <>
+            {lock && <LockUnlockModal value={String( goldyBalance ?? 0)} info={"Locking amount should not be more than the balance"} buttonName={"Lock Coin"} approvalAction={callApproval} createAction={callCreate} status={isTransactionSuccess} isLoading={isLoading} isVestingLoading={isVestingLoading} isTransactionLoading={isTransactionLoading} title={"Lock Tokens"}/>}
+            {unlock && <LockUnlockModal value={String(goldyBalance ?? 0)} info={"Unlocking amount should not be more than the locked balance"} buttonName={"Unlock Coin"} approvalAction={callApproval} createAction={callCreate} status={isTransactionSuccess} isLoading={isLoading} isVestingLoading={isVestingLoading} isTransactionLoading={isTransactionLoading} title={"Unlock Tokens"}/>}
+            <div className="flex flex-col mt-24 mx-16 bg-[#272727] rounded-md">
             <div className="container flex flex-row">
                 <div className="container basis-1/2 p-8">
                     <span className="font-bold text-[22px] leading-7 text-transparent bg-clip-text bg-gradient-to-r from-[#be9f59] to-[#433420]">Lock or Unlock Tokens For Maximum <br/><p className="text-[#be9f59]">Profits</p></span>
@@ -42,12 +110,17 @@ export default function Vesting() {
                 </div>
             </div>
             <div className="containerx flex flex-row space-x-6 justify-center mb-6">
-                <button className="bg-[#FFE082] px-24 py-4 text-black mt-3 rounded-md font-bold">
-                    <span>Lock Token</span>
-                </button>
-                <button className="bg-[#FFBC50] px-24 py-4 text-black mt-3 rounded-md font-bold">
-                    <span>Unlock Token</span>
-                </button>
+                <Link href="/portfolio?lock=true">
+                    <button className="bg-[#FFE082] px-24 py-4 text-black mt-3 rounded-md font-bold">
+                        <span>Lock Token</span>
+                    </button>
+                </Link>
+                <Link href="/portfolio?unlock=true">
+                    <button className="bg-[#FFBC50] px-24 py-4 text-black mt-3 rounded-md font-bold">
+                        <span>Unlock Token</span>
+                    </button>
+                </Link>
             </div>
-    </div>);
+    </div>
+        </>);
 }
